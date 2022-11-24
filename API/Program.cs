bf62d14ef4be09192be38daf3b747065a9836888
Bijin Kurien <sk.bijin@gmail.com>
2022-11-24 10:38:59 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,26 +1,38 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
-    public class Program
-    {
-        public static void Main(string[] args)
+        public class Program
         {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+                public static async Task Main(string[] args)
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+                        var host = CreateHostBuilder(args).Build();
+                        using (var scope = host.Services.CreateScope())
+                        {
+                                var services = scope.ServiceProvider;
+                                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                                try
+                                {
+                                        var context = services.GetRequiredService<StoreContext>();
+                                        await context.Database.MigrateAsync();
+                                        await StoreContextSeed.SeedAsync(context, loggerFactory);
+                                }
+                                catch (Exception ex)
+                                {
+                                        var logger = loggerFactory.CreateLogger<Program>();
+                                        logger.LogError(ex, "An error occured during migration !!");
+                                }
+                        }
+
+                        host.Run();
+                }
+
+                public static IHostBuilder CreateHostBuilder(string[] args) =>
+                    Host.CreateDefaultBuilder(args)
+                        .ConfigureWebHostDefaults(webBuilder =>
+                        {
+                                webBuilder.UseStartup<Startup>();
+                        });
+        }
 }
